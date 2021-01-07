76d407c8dcbabf0bf46bd84f87da800a8c2c9ea2
waouo <f5864565@gmail.com>
2021-01-07 13:58:05 +0800

    fix-screen-animation-flashing

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,4 @@
-import { cloneElement } from 'react'
+import { cloneElement, useEffect } from 'react'
 /* eslint-disable react/prop-types */
 import { Route, Switch, withRouter } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
@@ -6,27 +6,34 @@ import HomeScreen from './screens/HomeScreen'
 import TodoListScreen from './screens/TodoListScreen'
 import AnalyticsScreen from './screens/AnalyticsScreen'
 import MusicScreen from './screens/MusicScreen'
+import { createBrowserHistory } from 'history'
 
 const ANIMATION_MAP = {
   PUSH: 'forward',
   POP: 'back',
 }
 
-const Routes = withRouter(({ location, history }) => (
-  <TransitionGroup
-    childFactory={(child) =>
-      cloneElement(child, { classNames: ANIMATION_MAP[history.action] })
-    }
-  >
-    <CSSTransition timeout={500} key={location.pathname}>
-      <Switch location={location}>
-        <Route exact path="/" component={HomeScreen} />
-        <Route exact path="/todo" component={TodoListScreen} />
-        <Route exact path="/analytics" component={AnalyticsScreen} />
-        <Route exact path="/music" component={MusicScreen} />
-      </Switch>
-    </CSSTransition>
-  </TransitionGroup>
-))
+const Routes = withRouter(({ location, history }) => {
+  useEffect(() => {
+    console.log(history.action);
+  })
+
+  return (
+    <TransitionGroup
+      childFactory={(child) =>
+        cloneElement(child, { classNames: ANIMATION_MAP[history.action] })
+      }
+    >
+      <CSSTransition timeout={500} key={location.pathname}>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/" component={HomeScreen} />
+          <Route exact path="/todo" component={TodoListScreen} />
+          <Route exact path="/analytics" component={AnalyticsScreen} />
+          <Route exact path="/music" component={MusicScreen} />
+        </Switch>
+      </CSSTransition>
+    </TransitionGroup>
+  )
+})
 
 export default Routes
